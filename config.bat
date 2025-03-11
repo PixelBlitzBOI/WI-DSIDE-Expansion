740437cc4a57ef86ec94b32da036071d44fb72c9
PixelBlitzBOI <145938952+PixelBlitzBOI@users.noreply.github.com>
2025-03-11 16:52:22 -0700

    Update config.bat

--- config.bat
+++ config.bat
@@ -1,5 +1,8 @@
 @echo off
-title Installing Libraries
+title WIDSE Libraries Setup - Confirm
+echo Welcome to the WIDSE Libraries Setup! Press Enter to start the setup
+pause
+title WIDSE Libraries Setup - Installing Libraries
 haxelib set lime 8.0.2
 haxelib set flixel 4.11.0
 haxelib set flixel-addons 2.12.0
@@ -13,6 +16,6 @@ haxelib set hxp 1.2.2
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
 haxelib set flxanimate 1.2.0
 cls
-echo We are finished setting up your libraries!
+echo We are finished setting up your libraries! Press enter to quit
 pause >nul
 exit
